3360569033d00008ac92037088420786c16770b7
Silvija Prozinger <silvija.prozinger@gmail.com>
2018-08-05 19:48:24 +0200

    edited side drawer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Map from './components/Map.js';
 import Places from './components/Places.js';
 import Header from './components/Header.js';
+import SideDrawer from './components/SideDrawer.js'
 import './App.css';
 
 const API = 'https://api.foursquare.com/v2/venues/search?ll=45.5616873,18.6770196&intent=browse&radius=1000&query=cafe&client_id=PGIW2RVNRZZJO4GIBP1ALRK4T3ZK20H4WFHEY5X2OZB4C1MN&client_secret=LLUU3RJV0MHUXU2RSWDZ3RAPO00HFZRKVGU2AGGXUQB41DUY&v=20180728'
@@ -33,6 +34,7 @@ class App extends Component {
     return (
       <div className="App">
       <Header/>
+      <SideDrawer/>
         <div className="map">
         <Map center={location} markers={this.state.venues}/>
         </div>
---
+++ src/components/SideDrawer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+const SideDrawer = props => {
+  return (
+    <nav className="side-drawer">
+      <ul>
+        <li>
+          <a href="/">Venues</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default SideDrawer;
