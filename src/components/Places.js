bd4c950053778335ad531bd78a55a4a6eac48091
Silvija Prozinger <silvija.prozinger@gmail.com>
2018-08-07 15:54:10 +0200

    edited search box css

--- src/components/Places.js
+++ src/components/Places.js
@@ -13,13 +13,17 @@ class Places extends Component {
 		this.setState({search: event.target.value});
 
 	}
+
+	//filteredList variable filters the list of venues after user types into search box
 	render () {
 		let filteredList = this.props.venues.filter(
 			(venue) => {
+				//toLowerCase helps appropriate letter be recognized regardless of upper case or lower case
 				return venue.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
 			}
 
 		);
+		//variable list maps through list of venues and their id to generate their names
 		const list = filteredList.map((venue, i) => {
 			return (
 				<li key = {i}>{venue.name}</li>
@@ -27,13 +31,18 @@ class Places extends Component {
 		})
 
 		return (
+		<div className="sidebar-content">
+		 <div className="filter-box">
+			<input type="text" 
+			placeholder="Search for venues"
+			value={this.state.search} 
+			onChange={this.updateSearch.bind(this)}/>
+		  </div>
 		 <div className="places-list">
 		 	<h3>Venues:</h3>
-		 <ul>
+		 	<ul>
 		 	{list}
-		 </ul>
-		 <div className="filter-box">
-		 <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)}/>
+		 	</ul>
 		 </div>
 		 </div>
 
